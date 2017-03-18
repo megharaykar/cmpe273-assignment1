b26f517018b9a192305d891b4a6a8b0c82dc9a64
megharaykar <megharaykar24@gmail.com>
2017-03-17 22:20:38 -0700

    Updated app.py

--- cmpe273-lab2/app.py
+++ cmpe273-lab2/app.py
@@ -3,12 +3,8 @@ import sys,base64
 from github import Github
 app = Flask(__name__)
 outputstring=""
-@app.route("/")
-def hello():
-	output=outputstring.replace("\n","<br>")
-	return output
-
-if __name__ == "__main__":
+@app.route("/v1/<filename>")
+def getfilecontents(filename):
 	try:
 		arg1 = sys.argv[1]
 		val,repositoryurl=arg1.split("https://github.com/")
@@ -18,15 +14,14 @@ if __name__ == "__main__":
 		list_of_files=repository.get_dir_contents('')
 		for file in list_of_files:
 			filepath = file.path
-			if filepath.endswith("config.yml") or filepath.endswith("config.json"):
-				print(filepath)
+			if filepath.endswith(filename):
 				file_content = repository.get_contents(filepath)
 				file_data = base64.b64decode(file_content.content)
-				outputstring=outputstring+file_data
-				print(file_data)
-		print("The Final output is \n")
-		print(outputstring)
-		app.run(debug=False,host='0.0.0.0')
+				outputstring=file_data
 	except IndexError:
 		print("Usage: app.py <arg1>")
-		sys.exit(1)
+		sys.exit(1)			
+	return outputstring
+
+if __name__ == "__main__":
+		app.run(debug=False,host='0.0.0.0')
